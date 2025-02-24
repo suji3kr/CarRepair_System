9df48977685fdf18a368d165e88321f1ffaadef3
xdo13 <xdo13@naver.com>
2025-02-24 14:53:20 +0900

    프론트, 뺵 연결성공

--- GaragEZ/vite.config.ts
+++ GaragEZ/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    port : 3000
-  }
-})
+  server: {
+    port: 3000, // 프론트엔드 서버 포트 설정 (리액트 실행 포트)
+    proxy: {
+      '/api': 'http://localhost:8094', // '/api'로 시작하는 요청은 백엔드 서버(스프링부트)로 프록시
+    },
+  },
+});
