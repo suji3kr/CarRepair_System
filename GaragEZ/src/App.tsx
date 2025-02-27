9ce99b840115f9bbfb22fe25e595378b86f4f6c9
kuma_Hwang <kuuma0807@gmail.com>
2025-02-27 16:13:10 +0900

    드롭다운 메뉴 및 로그인창 구글로그인 가입 추가

--- GaragEZ/src/App.tsx
+++ GaragEZ/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google"; // Google OAuth 추가
 import Header from "./components/Header";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
@@ -24,20 +25,31 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Layout>
+    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/menu1" element={<MainPage />} />  
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/menu3" element={<ContactForm />} />
-          <Route path="/chat-bot" element={<ChatBot />} /> 
-          <Route path="/login" element={<Login />} /> 
+          <Route path="*" element={<LayoutWrapper />} />
         </Routes>
-      </Layout>
-    </BrowserRouter>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  );
+};
+
+// ✅ 모든 페이지를 `Layout`과 함께 렌더링하는 컴포넌트
+const LayoutWrapper: React.FC = () => {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/menu1" element={<MainPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/menu3" element={<ContactForm />} />
+        <Route path="/chat-bot" element={<ChatBot />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Layout>
   );
 };
 
