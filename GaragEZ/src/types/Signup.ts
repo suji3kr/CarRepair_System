99a4a4b5c5bdf76274af8501b0c2b5827cbfb838
suji3kr <cutesuji3kr@naver.com>
2025-03-07 16:31:44 +0900

    Sign up.ts 수정

--- GaragEZ/src/types/Signup.ts
+++ GaragEZ/src/types/Signup.ts
@@ -1,14 +1,21 @@
 // Spring Boot로 보낼 회원가입 데이터 타입 정의
 export interface FormData {
+  // 사용자
   user_id: string;
   password: string;
   name: string;
   email: string;
   phone: string;
+  // 차량
+  carMake: string;
   carModel: string;
   carNumber: string;
+  year: number;
+  vin: string;
+  //공동소유자
   coOwner: boolean;
   coOwnerName: string;
   coOwnerPhone: string;
+  
   termsAgreed: boolean;
 }
