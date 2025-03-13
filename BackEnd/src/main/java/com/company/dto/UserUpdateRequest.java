7e9484eb7f5b1d67b846523679aa5b061998c53d
kuma_Hwang <kuuma0807@gmail.com>
2025-03-13 17:45:32 +0900

    회원가입 창 공동소유주 등록 시 경고문구 추가 및 input 간격 수정

--- BackEnd/src/main/java/com/company/dto/UserUpdateRequest.java
+++ BackEnd/src/main/java/com/company/dto/UserUpdateRequest.java
@@ -1,11 +1,12 @@
 package com.company.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class UserUpdateRequest {
     private String name;
+    @Email(message = "유효한 이메일 형식이어야 합니다.")
+    private String email;
     private String phone;
 }
